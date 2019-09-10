24552b99f9ade9fe43b39633df5435d52dc5ffed
Matthew Myers <mmyers@welf.net>
2019-09-09 22:25:10 -0700

    added more testing functionality to install.sh.

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-if [ -w "$HOME/.zshrc" ]; then
-    printf '%s' 'Overwrite existing zshrc? (y/n) '
-    read ANSWER
-
-    case $ANSWER in
-        [yY]) rm "$HOME/.zshrc" && ln -s "$PWD/zshrc" "$HOME/.zshrc" ;;
-    esac
+if [ ! $(command -v zsh) ]; then
+    echo "zsh is not installed, skipping.";
+else 
+    if [ -w "$HOME/.zshrc" ]; then
+        printf '%s' 'Overwrite existing zshrc? (y/n) '
+        read ANSWER
+        case $ANSWER in
+            [yY]) 
+                echo "overwriting zshrc"
+                rm "$HOME/.zshrc" 
+                ln -s "$PWD/zshrc" "$HOME/.zshrc"
+                ;;
+        esac
+    fi
 fi
