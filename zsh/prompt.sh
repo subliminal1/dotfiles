902ab9a86c28ca69496fb34dec05b12955bea1d1
Matthew Myers <mmyers@welf.net>
2020-03-30 07:42:43 -0700

    ZSH fix RPROMPT

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -1,5 +1,14 @@
 # vim: filetype=sh
 
+autoload -Uz vcs_info
+
+precmd() {
+    vcs_info
+    if [[ -n ${vcs_info_msg_0_} ]]; then
+        RPROMPT="${vcs_info_msg_0_}"
+    fi
+}
+
 # Prompt symbol
 COMMON_PROMPT_SYMBOL="❯"
 
@@ -42,33 +51,6 @@ common_return_status() {
   echo -n "%(?.%F{$COMMON_COLORS_RETURN_STATUS_TRUE}.%F{$COMMON_COLORS_RETURN_STATUS_FALSE})$COMMON_PROMPT_SYMBOL%f "
 }
 
-# Git status
-common_git_status() {
-    local message=""
-    local message_color="%F{$COMMON_COLORS_GIT_STATUS_DEFAULT}"
-
-    # https://git-scm.com/docs/git-status#_short_format
-    local staged=$(git status --porcelain 2>/dev/null | grep -e "^[MADRCU]")
-    local unstaged=$(git status --porcelain 2>/dev/null | grep -e "^[MADRCU? ][MADRCU?]")
-
-    if [[ -n ${staged} ]]; then
-        message_color="%F{$COMMON_COLORS_GIT_STATUS_STAGED}"
-    elif [[ -n ${unstaged} ]]; then
-        message_color="%F{$COMMON_COLORS_GIT_STATUS_UNSTAGED}"
-    fi
-
-    local branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-    if [[ -n ${branch} ]]; then
-        message+="${message_color}${branch}%f"
-    fi
-
-    echo -n "${message}"
-}
-
-# Git prompt SHA
-ZSH_THEME_GIT_PROMPT_SHA_BEFORE="%{%F{$COMMON_COLORS_GIT_PROMPT_SHA}%}"
-ZSH_THEME_GIT_PROMPT_SHA_AFTER="%{$reset_color%} "
-
 # Background Jobs
 common_bg_jobs() {
   bg_status="%{$fg[$COMMON_COLORS_BG_JOBS]%}%(1j.↓%j .)"
@@ -76,4 +58,3 @@ common_bg_jobs() {
 }
 
 PROMPT="$(common_host)$(common_current_dir)$(common_bg_jobs)$(common_return_status)"
-RPROMPT="$(common_git_status)"
